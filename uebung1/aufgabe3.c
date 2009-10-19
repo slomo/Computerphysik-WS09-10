e58aa1e68e082f7767c1f854a2db715be31ed5a8
Yves <steve.harrison@gmx.net>
2009-10-19 11:23:33 +0200

    uebung1: aufgaben 3 und 4 fertig (evntl. weitere tests auf der 4 nötig)

---
+++ uebung1/aufgabe3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc,char *args[]) {
+    char* string = malloc(sizeof(char)*81);
+    int i;
+
+    fscanf(stdin,"%80s",string);
+    
+    for(i=80;i>=0;i--){
+        putc(string[i],stdout);
+    }
+    putc('\n',stdout);
+}
---
+++ uebung1/aufgabe4.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<math.h>
+
+#define MAXSAMPELS 5
+// this programm has to be linked with -lm options
+
+int main(int argc,char *args[]){
+    double avg=0,stddev=0,set[4];
+    int i;
+    
+    fprintf(stdout,"Please enter your sampels:\r\n");
+    for(i=0;i<MAXSAMPELS;i++){
+        fprintf(stdout,"Sample No. %d: ",(i+1));
+        fscanf(stdin,"%lf",(set+i));
+    }
+    
+    // compute avarage
+    for(i=MAXSAMPELS;i>=0;i--){
+        avg += set[i];
+    }
+    avg /= MAXSAMPELS;
+    fprintf(stdout,"The Avarge is: %e\r\n",avg);
+
+    // compute standart devitation
+    for(i=0;i<5;i++){
+        stddev += pow((set[i] - avg),2);
+    }
+    stddev /= MAXSAMPELS-1;
+    stddev = sqrt(stddev);
+    fprintf(stdout,"The standart devitation is: %e\r\n",stddev);
+}
+
+
