91f5479a144b56db4d0bbf23055fa66f4c107ef4
Robin Nehls <nehls@mi.fu-berlin.de>
2009-10-19 13:28:52 +0200

    Fixed warnings

--- uebung1/aufgabe1.c
+++ uebung1/aufgabe1.c
@@ -3,10 +3,10 @@
 int main(int argc,char* args[]) {
     fprintf(stdout,"Size of some datatypes\r\n");
     fprintf(stdout,"=================================================\r\n");
-    fprintf(stdout,"\t character: \t %d byte\r\n",sizeof(char));
-    fprintf(stdout,"\t short int: \t %d byte\r\n",sizeof(short));
-    fprintf(stdout,"\t interger: \t %d byte\r\n",sizeof(int));
-    fprintf(stdout,"\t double: \t %d byte\r\n",sizeof(double));
+    fprintf(stdout,"\t character: \t %ld byte\r\n",sizeof(char));
+    fprintf(stdout,"\t short int: \t %ld byte\r\n",sizeof(short));
+    fprintf(stdout,"\t interger: \t %ld byte\r\n",sizeof(int));
+    fprintf(stdout,"\t double: \t %ld byte\r\n",sizeof(double));
 
     return 0;
 }
