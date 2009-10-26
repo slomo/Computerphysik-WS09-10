0bfc2318cadd6591b4cd438527c839478387deee
Yves <steve.harrison@gmx.net>
2009-10-26 17:04:30 +0100

    Aufgabe 2 nun exakt gelöst,die Implemntierung mit einigen Kommentaren erläutert

--- uebung2/aufgabe2.c
+++ uebung2/aufgabe2.c
@@ -3,32 +3,43 @@
 #include <math.h>
 
 #define STEP 10
-#define LIMIT pow(10,8)
+#define LIMIT powl(10,9)
+
+/* 
+ * We took the formular for the exact value from wikipedia.
+ * http://en.wikipedia.org/wiki/Harmonic_series_(mathematics)
+ *
+ * The constant value is an approximation of the Euler–Mascheroni constant.
+ *
+ */
 
 int main(int argc,char *args[]){
     
-    int down=pow(10,8),up=1,count=10;
+    long long int i,down,up;
     FILE* out;
-    double sumUp=0,sumDown=0;
+    double sumUp,sumDown;
 
-    out = fopen("output.tmp","a");
+    out = fopen("output.tmp","w");
     
-    while(down > 0){
-        sumUp +=  1/((double) up);
-        sumDown +=  1/((double) down);
-        
-        up ++;
-        down --;
-        if(up==count){
-            fprintf(out,"[%d][%d]\r\n\
-                    \tSup: %E\r\n\
-                    \tSdown: %E\r\n"
-                ,up,down,sumUp,sumDown);
-            fprintf(stdout,"Results have been writen to file\r\n");
-            count *= 10;
+    for(i=10;i<LIMIT;i *=10){
+        sumUp=0;
+        sumDown=0;
+
+        for(down=i;down>0;down --){
+            sumDown +=  1/((double) down);
         }
+        for(up=0;up<i;up++){
+            sumUp +=  1/(up+1.0);
+        }
+        
+        fprintf(out,"[%lld][%lld]\r\n\
+            \tSup: \t%.9E\r\n\
+            \tSdown: \t%.9E\r\n\
+            \tExact: \t%.9E\r\n"
+            ,up,down,sumUp,sumDown,log(i)+0.5772156649);
+        fprintf(stdout,"Results have been writen to file\r\n");
     }
-    fprintf(out,"\tExpected: %E\r\n",log(up)+0.5772156649);
+    
 
     fclose(out);
     return EXIT_SUCCESS;
