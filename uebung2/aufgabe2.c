0fc2b445b086bbb960a9fc58b0526cdaf94e5893
Yves <steve.harrison@gmx.net>
2009-10-26 11:53:33 +0100

    Blatt2 Aufgabe2 funktioniert, entsprocht allerding snicht ganz de Aufgabenstellung, will be fixed soon

--- uebung2/aufgabe2.c
+++ uebung2/aufgabe2.c
@@ -1,26 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+#define STEP 10
+#define LIMIT pow(10,8)
 
 int main(int argc,char *args[]){
     
-    int limit,n;
-    FILE* out = malloc(sizeof(struct file));
+    int down=pow(10,8),up=1,count=10;
+    FILE* out;
     double sumUp=0,sumDown=0;
 
-    limit = atoi(args[1]); // read n from argument 1
-    //Sup
-    for(n=1;n<=limit;n++){
-        sumUp +=  1/n;
+    out = fopen("output.tmp","a");
+    
+    while(down > 0){
+        sumUp +=  1/((double) up);
+        sumDown +=  1/((double) down);
+        
+        up ++;
+        down --;
+        if(up==count){
+            fprintf(out,"[%d][%d]\r\n\
+                    \tSup: %E\r\n\
+                    \tSdown: %E\r\n"
+                ,up,down,sumUp,sumDown);
+            fprintf(stdout,"Results have been writen to file\r\n");
+            count *= 10;
+        }
     }
-
-    //Sdown
-    for(n=limit;n>0;n++){
-        sumUp +=  1/n;
-    }
-
-    out = fopen("output.tmp","w");
-    fprintf(out,"Sup: %E\nSdown: %E\n",sumUp,sumDown);
-    fprintf(stdout,"Results have been writen to file");
+    fprintf(out,"\tExpected: %E\r\n",log(up)+0.5772156649);
 
     fclose(out);
     return EXIT_SUCCESS;
