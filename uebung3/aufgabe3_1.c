ef084a9280b3e9fdfdaaf3b23f7cfd60e7df8730
Robin Nehls <nehls@mi.fu-berlin.de>
2009-11-02 12:00:20 +0100

    Added aufgabe3_1.c (does not compile crazy errors plz fix)

---
+++ uebung3/aufgabe3_1.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+#define A = -1
+#define B = 1
+#define X = pi() /4
+
+double f(double x)
+{
+    return 5/(1+25*x*x);
+}
+
+void distribute_linear(double *x, int n)
+{
+    int i;
+    for(i=0;i<=n;++i){
+        x[i]=A+i*((B-A)/n);
+    }
+}
+
+void distribute_tschebyscheff(double *x, int n)
+{ 
+    int i;
+    for(i=0;i<=n;++i){
+        x[i]=-cos(((i+0.5)/(n+1))*pi());
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    int n = atoi(argv[2]);
+    int i,j;
+    double p;
+    double t[22];
+    double a[22];
+    double x[22];
+    /*
+    double *t = malloc(sizeof(double)*n);
+    double *a = malloc(sizeof(double)*n);
+    double *x = malloc(sizeof(double)*n);
+    */
+
+    if(argc==3) {
+        if(!strcmp(argv[1],"-l")) {
+            distribute_linear(x,n);
+        }
+        if(!strcmp(argv[1],"-t")) {
+            distribute_tschebyscheff(x,n);
+        }
+    
+        //Algo Start
+        for(i=0;i<=n;++i) {
+            t[i]=f(x[i]);
+            if(i>=1) {
+                for(j=i-1;j>=0;--j) {
+                    t[x]=(t[j+1]-t[j])/(x[i]-x[j]);
+                }
+            }
+            a[i]=t[0];
+        }
+
+        p=a[n];
+        for(i=n-1;i>=0;--i) {
+            p=p*(X-x[i])+a[i];
+        }
+
+        //Algo Stop
+
+        printf("n=%d\nx=%f\np=%f\n",n,X,p);
+
+        /*
+        free(t);
+        free(a);
+        free(x);
+        */
+    }
+    else {
+        printf("Usage: %s <-l|-t> <n>\n-l Linear distribution\n-t Tschebyscheff distribution\n",argv[0]);
+        /*
+        free(t);
+        free(a);
+        free(x);
+        */
+    }
+
+    return EXIT_SUCCESS;
+}
