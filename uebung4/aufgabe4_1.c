cd6a2652a757996b938a4f18fb2727933f7fc32a
Yves <steve.harrison@gmx.net>
2009-11-15 20:29:03 +0100

    final edits on task 1

--- uebung4/aufgabe4_1.c
+++ uebung4/aufgabe4_1.c
@@ -8,7 +8,7 @@
 #define B 1.0
 #define PROBEC 100.0
 #define PI 3.14159265
-#define X1 B/n
+#define X1 B/(double)n
 
 /*
  * Wee took some parts auf that code from our solution of taks 2 on 
@@ -56,6 +56,8 @@ void compute_polynomial(double *x,double *t,int n,double *a){
 
 
 double compute_horner(double probe,double *x,double *a,int n){
+
+    
     // modified caused by smaller interval (first value is not included) 
     double p=a[n-1];
     int i;
@@ -66,33 +68,33 @@ double compute_horner(double probe,double *x,double *a,int n){
     return p;
 }
 
-double compute_faktor(double s, double e,int n){
+double compute_faktor(int n){
     // get faktor (see .pdf for additional info)
-    double max=-DBL_MAX,tmp=max,xi;
-    double faktor = 1;
+    double faktor,xi;
     int i;
-    faktor = (-1) * ((n+1)%2); // efficient way for (-1)^n+1
+    faktor = ((n+1)%2)==0 ? 1 : -1; // efficient way for (-1)^n+1
     for(i=1;i<=(n+1);i++){
         faktor *= 1.0-(3.0/(2.0*i));
     }
     
-    for(xi=s;xi<=e;xi=+(X1/PROBEC)){
-        tmp = faktor * pow(xi,(n+1)-0.5);
+    return faktor;
+}
+
+double compute_max_err(double tilde,double faktor,int n,double *x){
+    double xi,max=-DBL_MAX,tmp=max;
+    int i;
+    
+    for(xi=tilde;xi<=B;xi+=(X1/PROBEC)){
+        tmp = faktor * powf(xi,((2.0*n)-1.0)*(-0.5));
         if(tmp > max){
             max = tmp;
         }
     }
 
-    return faktor;
-}
-
-double compute_max_err(double s,double tilde,double faktor,int n){
-    double xi;
-
-    for(xi=s;xi<tilde;xi=+(X1/PROBEC)){
-        faktor*=xi;
+    for(i=0;i<n;i++){
+        max*=(tilde - x[i]);
     }
-    return(faktor);
+    return(max);
 }
 
 int main(int argc, char* argv[])
@@ -114,25 +116,26 @@ int main(int argc, char* argv[])
         x = malloc((sizeof(double)*(n)));
         
         if(0<n && n<100){
-            filename = malloc(sizeof("xxx.ZZ.dat"));
+            filename = malloc(sizeof(char)*255);
         }
         else{
             printf("Usage: %s <n>, where 0<n<100\r\n",argv[0]);
         }
 
-        strcat(filename,"err.");
+        filename[0] = '\0';
+        strcat(filename,"data/err.");
         strcat(filename,argv[1]);
         strcat(filename,".dat");
         fErr = fopen(filename,"w");
 
         filename[0] = '\0';
-        strcat(filename,"res.");
+        strcat(filename,"data/res.");
         strcat(filename,argv[1]);
         strcat(filename,".dat");
         fRes = fopen(filename,"w");
 
         filename[0] = '\0';
-        strcat(filename,"maxError.");
+        strcat(filename,"data/maxError.");
         strcat(filename,argv[1]);
         strcat(filename,".dat");
         fMaxError = fopen(filename,"w");
@@ -142,23 +145,26 @@ int main(int argc, char* argv[])
         compute_polynomial(x,t,n,a);
         
         // computing maax error
-        faktor = compute_faktor(A+(X1/PROBEC),X1,n);
+        faktor = compute_faktor(n);
 
         for(probe=A+(X1/PROBEC);probe<=X1;probe+=(X1/PROBEC)){
             result = compute_horner(probe,x,a,n);
             fprintf(fRes,"%.20E %.20E\r\n",probe,result);
             fprintf(fErr,"%.20E %.20E\r\n",probe,fabs(result-f(probe)));
             fprintf(fMaxError,"%.20E %.20E\r\n",probe,
-                    compute_max_err(A+(X1/PROBEC),probe,faktor,n));
+                    compute_max_err(probe,faktor,n,x));
         }
         for(;probe<=B;probe+=(X1/PROBEC)){
+            result = compute_horner(probe,x,a,n);
             fprintf(fRes,"%.20E %.20E\r\n",probe,result);
         }
         
 
         fclose(fRes);
         fclose(fErr);
-        
+        fclose(fMaxError);
+
+        free(filename);
         free(t);
         free(a);
         free(x);
