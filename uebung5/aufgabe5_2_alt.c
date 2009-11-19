08dfbacecf5a26e16db096e61d6fdd8852b72dc1
Robin Nehls <nehls@mi.fu-berlin.de>
2009-11-19 10:00:42 +0100

    Added test programm using fftw and fixed of by one in aufgabe5_2.c

--- uebung5/aufgabe5_2_alt.c
+++ uebung5/aufgabe5_2_alt.c
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fftw3.h>
 
 #define N 100
 
@@ -19,36 +20,40 @@ void distribute_linear(double lower,double upper,double *x, int n)
 
 int main(int argc,char *argv[]){
 
-    int m,l;
+    int m;
     double h[N],x[N];
-    double complex hFouier[N];
-    
+ 
+    fftw_complex *in, *out;
+    fftw_plan p;
+    in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+    out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+
     FILE *fhl, *fhm; 
 
     distribute_linear((-4*M_PI),(4*M_PI),x,N);
 
-    for(l=0;l<N;l++){
-        h[l] = f(x[l]);
+    for(m=0;m<N;m++){
+        h[m] = f(x[m]);
+        in[m] = h[m];
     }
 
     fhl = fopen("vektor.data","w");
     fhm = fopen("fouier.data","w");
 
-    for(m=1;m<N;m++){
-        hFouier[m]=0;
-        for(l=1;l<N;l++){
-            hFouier[m] += (h[l]*cos(2*l*m*M_PI/N)) + (h[l]*sin(2*l*m*M_PI/N))*I;
-            
-        }
+    p = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+    fftw_execute(p);
+    fftw_destroy_plan(p);
 
-        printf("x:%f \thl:%f \tHm:%f + %fi\n",x[m],h[m]
-                ,creal(hFouier[m]),cimag(hFouier[m]));
+    for(m=1;m<N;m++){
+        printf("x:%f \thl:%f \tHm:%f + %fi\n",x[m],h[m],creal(out[m]),cimag(out[m]));
         
         fprintf(fhl,"%f %f\n",x[m],h[m]);
         //fprintf(fhm,"%f {%f,%f}\n",x[m],creal(hFouier[m]),cimag(hFouier[m]));
-        fprintf(fhm,"%f %f\n",x[m],creal(hFouier[m]));
+        fprintf(fhm,"%f %f\n",x[m],creal(out[m]));
 
     }
+    fftw_free(in);
+    fftw_free(out);
     
     return EXIT_SUCCESS;
 }
