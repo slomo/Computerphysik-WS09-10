675f1010e2af7f06fdec4b16a997ddcd8da37b23
Robin Nehls <nehls@mi.fu-berlin.de>
2009-11-29 22:03:34 +0100

    Added output and plot of absolute error

--- uebung5/aufgabe5_1.c
+++ uebung5/aufgabe5_1.c
@@ -5,27 +5,7 @@
 
 #define A -5.0
 #define B 5.0
-#define PI 3.14159265
-#define X (PI/4)
 #define N 4
-
-/*
- * The value PI/4 is close to the borders of the interval, so the 
- * oscillation can become a disturbing factor, especally with 
- * linear distribution.
- * The rounded value of f(PI/4) is 0.30448
- * Here now the results for our interpolation
- *
- * #IValues p(X)lin delta   p(X)tsh delta
- * 5       -0.21491 0.51839 0.19445 0.11003
- * 12       2.69779 2.39331 0.17126 0.13322
- * 22       5.13411 4.82963 0.32770 0.02186
- *
- * It cann be seen that the best approch to the actual value comes
- * with tschebyscheff distribution and 22 interpolation vaules, wich
- * goes along with the results from task 2.
- */
-
 
 double f(double x)
 {
@@ -40,38 +20,15 @@ void distribute_linear(double *x, int n)
     }
 }
 
-void compute_polynomial(double *x,double *t,int n,double *a){
-    int i,j;
-
-    for(i=0;i<=n;++i) {
-        t[i]=f(x[i]);
-        if(i>=1) {
-            for(j=i-1;j>=0;--j) {
-                t[j]=(t[j+1]-t[j])/(x[i]-x[j]);
-            }
-        }
-            a[i]=t[0];
-    }
-}
-
-double compute_horner(double *x,double *a,int n){
-    double p=a[n];
-    int i;
-
-    for(i=n-1;i>=0;--i) {
-        p=p*(X-x[i])+a[i];
-    }
-    return p;
-}
-
 int main(int argc, char* argv[])
 {
     double probes[100],a[4][4],x[4]={-2,-1,1,2},result,y[4],h[4];
     int i,j=0;
-    FILE *fallout,*fsollout;
+    FILE *fallout,*fsollout,*ferrout;
 
     fallout = fopen("aufgabe5_1.out","w");
     fsollout = fopen("aufgabe5_1_soll.out","w");
+    ferrout = fopen("aufgabe5_1_err.out","w");
 
     distribute_linear(probes,100);
 
@@ -90,7 +47,7 @@ int main(int argc, char* argv[])
     }
 
     for(i=0;i<=2;i++) {
-        a[i][0]=f(x[i]);
+        a[i][0]=y[i];
         a[i][1]=(y[i+1]-y[i])/h[i+1] - (2*M[i]+M[i+1])/6 * h[i+1];
         a[i][2]=M[i]/2;
         a[i][3]=(M[i+1]-M[i])/(6*h[i+1]);
@@ -103,10 +60,12 @@ int main(int argc, char* argv[])
         result=a[j][0]+a[j][1]*(probes[i]-x[j])+a[j][2]*pow((probes[i]-x[j]),2)+a[j][3]*pow((probes[i]-x[j]),3);
         fprintf(fallout,"%f %f\n",probes[i],result);
         fprintf(fsollout,"%f %f\n",probes[i],f(probes[i]));
+        fprintf(ferrout,"%f %f\n",probes[i],result-f(probes[i]));
     }
 
     fclose(fallout);
     fclose(fsollout);
+    fclose(ferrout);
 
 return EXIT_SUCCESS;
 }
